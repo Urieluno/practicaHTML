ac353435fe02c60615a44816562c1b2105ecf632
Urieluno <urielperrenfernandez@gmail.com>
2022-05-07 23:57:42 -0300

    mi primer html

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) )
+
+
+app.listen(3000, () => {
+    console.log('El servidor corriendo en el puerto 3000');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./public/views/home.html'))
+})
